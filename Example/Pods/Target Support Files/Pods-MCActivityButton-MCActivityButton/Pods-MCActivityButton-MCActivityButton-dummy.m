79c2a829ba0289b4b10457a14fb796548c8f8022
Marcos Curvello <mrcurvello@gmail.com>
2015-05-10 23:02:19 -0300

    chore(example): pod update

---
+++ Example/Pods/Target Support Files/Pods-MCActivityButton-MCActivityButton/Pods-MCActivityButton-MCActivityButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MCActivityButton_MCActivityButton : NSObject
+@end
+@implementation PodsDummy_Pods_MCActivityButton_MCActivityButton
+@end
